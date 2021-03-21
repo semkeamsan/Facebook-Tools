391e229a90d416b79060722e39ae1eb67e97e764
semkeamsan <keamsan.sem@gmail.com>
2021-03-21 21:27:40 +0700

    update hindi generate phone

---
+++ json/hindi_operator.js
@@ -0,0 +1,140 @@
+const hindi_operator = {
+    "hindi": [{
+            "operator": "0955",
+            "length": "7"
+        },
+        {
+            "operator": "0906",
+            "length": "7"
+        },
+        {
+            "operator": "0915",
+            "length": "7"
+        },
+        {
+            "operator": "0916",
+            "length": "7"
+        },
+        {
+            "operator": "0917",
+            "length": "7"
+        },
+        {
+            "operator": "0925",
+            "length": "7"
+        },
+        {
+            "operator": "0926",
+            "length": "7"
+        },
+        {
+            "operator": "0927",
+            "length": "7"
+        },
+        {
+            "operator": "0935",
+            "length": "7"
+        },
+        {
+            "operator": "0936",
+            "length": "7"
+        },
+        {
+            "operator": "0937",
+            "length": "7"
+        },
+        {
+            "operator": "0996",
+            "length": "7"
+        },
+        {
+            "operator": "0997",
+            "length": "7"
+        },
+        {
+            "operator": "0922",
+            "length": "7"
+        },
+        {
+            "operator": "0923",
+            "length": "7"
+        },
+        {
+            "operator": "0932",
+            "length": "7"
+        },
+        {
+            "operator": "0933",
+            "length": "7"
+        },
+        {
+            "operator": "0907",
+            "length": "7"
+        },
+        {
+            "operator": "0908",
+            "length": "7"
+        },
+        {
+            "operator": "0909",
+            "length": "7"
+        },
+        {
+            "operator": "0910",
+            "length": "7"
+        },
+        {
+            "operator": "0912",
+            "length": "7"
+        },
+        {
+            "operator": "0918",
+            "length": "7"
+        },
+        {
+            "operator": "0919",
+            "length": "7"
+        },
+        {
+            "operator": "0920",
+            "length": "7"
+        },
+        {
+            "operator": "0921",
+            "length": "7"
+        },
+        {
+            "operator": "0928",
+            "length": "7"
+        },
+        {
+            "operator": "0929",
+            "length": "7"
+        },
+        {
+            "operator": "0930",
+            "length": "7"
+        },
+        {
+            "operator": "0938",
+            "length": "7"
+        },
+        {
+            "operator": "0939",
+            "length": "7"
+        },
+        {
+            "operator": "0989",
+            "length": "7"
+        },
+        {
+            "operator": "0999",
+            "length": "7"
+        },
+        {
+            "operator": "0948",
+            "length": "7"
+        },
+
+    ]
+}
